20f3f64443581c2619014f28c3a83dec8dac36c2
Ubeyid Oumer <ubeyidah@gmail.com>
2025-09-04 14:13:32 +0300

    fix: correct the css class name for .dark and remove card stats

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,11 @@
-import { ChartAreaInteractive } from "@/components/chart-area-interactive";
 import { DataTable } from "@/components/data-table";
-import { SectionCards } from "@/components/section-cards";
 import React from "react";
 import data from "./data.json";
-
 const Dashboard = () => {
   return (
     <div className="@container/main flex flex-1 flex-col gap-2">
       <div className="flex flex-col gap-4 py-4 md:gap-6 md:py-6">
-        <SectionCards />
+        {/* <SectionCards /> */}
         <div className="px-4 lg:px-6"></div>
         <DataTable data={data} />
       </div>
